2609d944453e2034335696581a299b0604683cab
toda-row <yoshihiro.t.88@gmail.com>
2016-09-17 21:48:18 +0900

    first
    test

---
+++ jquery.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  var wwidth = $(window).width();
+  if(wwidth >= 1000){
+    $("#slider img").attr({width: "100%"});
+  }else{
+    $("#slider img").attr({width: "1000px"});
+  }
+});
+$(window).resize(function(){
+  var wwidth = $(window).width();
+  if(wwidth >= 1000){
+    $("#slider img").attr({width: "100%"});
+    }else{
+    $("#slider img").attr({width: "1000px"});
+  }
+});
+
+
+$(function(){
+    var setImg = '#slider';
+    var fadeSpeed = 1000;
+    var switchDelay = 3500;
+ 
+    $(setImg).children('img').css({opacity:'0'});
+    $(setImg + ' img:first').stop().animate({opacity:'1',zIndex:'20'},fadeSpeed);
+ 
+    setInterval(function(){
+        $(setImg + ' :first-child').animate({opacity:'0'},fadeSpeed).next('img').animate({opacity:'1'},fadeSpeed).end().appendTo(setImg);
+    },switchDelay);
+});
+
+
